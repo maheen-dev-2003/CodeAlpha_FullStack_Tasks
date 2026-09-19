909212fc1c4ad92c4c1b2a848e0e164958364b19
Maheen <maheensaeed2003@gmail.com>
2026-09-19 19:26:42 +0500

    Create Project model

---
+++ Project_Management_Tool/Project.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    description: {
+      type: String,
+      trim: true
+    },
+
+    status: {
+      type: String,
+      enum: ["Planning", "In Progress", "Completed"],
+      default: "Planning"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Project", projectSchema);
