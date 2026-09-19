662dc54a96a28d2376726fe1924a4ca67e0222ed
Maheen <maheensaeed2003@gmail.com>
2026-09-19 11:45:33 +0500

    Connect database to server
    Added database connection setup to server.

--- Project_Management_Tool/server.js
+++ Project_Management_Tool/server.js
@@ -2,8 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./db");
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 // Middleware
 app.use(cors());
