b2fb0b3fd9c5ae69e8ab85a017d39ddb221fe0ed
Maheen <maheensaeed2003@gmail.com>
2026-09-19 18:49:24 +0500

    Add Task model

---
+++ Project_Management_Tool/Task.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    description: {
+      type: String,
+      trim: true
+    },
+
+    status: {
+      type: String,
+      enum: ["To Do", "In Progress", "Completed"],
+      default: "To Do"
+    },
+
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium"
+    },
+
+    dueDate: {
+      type: Date
+    },
+
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
